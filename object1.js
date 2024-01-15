40923bd5466d39908ffe25b07d1c1bf3f5385e70
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2024-01-15 15:54:11 +0530

    Create object1.js

---
+++ object1.js
@@ -0,0 +1,13 @@
+//object
+const circle = {
+    radius : 1,
+    location : {
+        x : 1,
+        y : 1
+    },
+    isVisible : true,
+    draw : function(){
+        console.log("draw");
+    }
+};
+circle.draw();
